42d26706893fd2e7652ae12071061a8317e9506d
Ishimwe Divin <ishimwedivin2@gmail.com>
2025-03-04 10:09:16 -0800

    modify classes

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,6 +1,16 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
 const AdminPage = () => {
-    return <h1>Admin Page</h1>;
-  };
-  
-  export default AdminPage;
-  
+  return (
+    <div className="flex flex-col min-h-screen bg-gray-200">
+      <Header />
+      <div className="flex flex-col items-center justify-center flex-grow">
+        <h1 className="text-3xl font-bold">Admin Page</h1>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default AdminPage;
--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,10 +1,16 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
 const UserPage = () => {
-    return (
-      <div className="h-screen flex items-center justify-center bg-gray-200">
+  return (
+    <div className="flex flex-col min-h-screen bg-gray-200">
+      <Header />
+      <div className="flex flex-col items-center justify-center flex-grow">
         <h2 className="text-3xl font-bold">User Dashboard</h2>
       </div>
-    );
-  };
-  
-  export default UserPage;
-  
+      <Footer />
+    </div>
+  );
+};
+
+export default UserPage;
